6743e60e9c2de681eddee5d3f8d95884bcd520e5
Joseph Caudle <joseph.caudle@gmail.com>
2012-04-25 00:55:41 -0500

    add first spec

---
+++ hello/1/greeter_spec.rb
@@ -0,0 +1,7 @@
+describe "RSpec Greeter" do
+  it "should say 'Hello RSpec!' when it receives the greet() message" do
+    greeter = RSpecGreeter.new
+    greeting = greeter.greet
+    greeting.should == "Hello RSpec!"
+  end
+end
