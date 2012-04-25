3c9db2ec79ef6e7aa3ae99cc81f870faf39ba333
Joseph Caudle <joseph.caudle@gmail.com>
2012-04-25 01:10:21 -0500

    Added RSpecGreeter class

--- hello/greeter_spec.rb
+++ hello/greeter_spec.rb
@@ -1,3 +1,9 @@
+class RSpecGreeter
+  def greet
+    "Hello RSpec!"
+  end
+end
+
 describe "RSpec Greeter" do
   it "should say 'Hello RSpec!' when it receives the greet() message" do
     greeter = RSpecGreeter.new
